88283a6271c6d9995c842f52577af8b516deb5a4
Mikhail Belyaev <belyaev@kspt.icc.spbstu.ru>
2017-07-25 18:55:27 +0300

    m

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,9 @@ ant -f build.xml
 mvn deploy:deploy-file \
     -Dfile=dist/kotlin-compiler-before-shrink.jar \
     -DgroupId=org.jetbrains.kotlin \
-    -DartifactId=kotlin-compiler-full \
+    -DartifactId=kotlin-compiler-nostrip \
     -Dversion=1.1.3 \
     -Dpackaging=jar \
-    -Durl="https://api.bintray.com/maven/vorpal-research/kotlin-maven/ktuples/;publish=1" \
+    -Durl="https://api.bintray.com/maven/vorpal-research/kotlin-maven/kotlin-compiler-nostrip/;publish=1" \
     -DrepositoryId=bintray-vorpal-research-kotlin-maven
 
