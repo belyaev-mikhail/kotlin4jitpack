38aed0f360a8e374e8307d04bff2ed480d552cce
Mikhail Belyaev <belyaev@kspt.icc.spbstu.ru>
2017-07-25 20:10:14 +0300

    m

--- deploy.sh
+++ deploy.sh
@@ -10,3 +10,14 @@ mvn deploy:deploy-file \
     -Durl="https://api.bintray.com/maven/vorpal-research/kotlin-maven/kotlin-compiler-nostrip/;publish=1" \
     -DrepositoryId=bintray-vorpal-research-kotlin-maven
 
+mvn deploy:deploy-file \
+    -Dfile=dist/kotlin-compiler-sources.jar \
+    -DgroupId=org.jetbrains.kotlin \
+    -DartifactId=kotlin-compiler-nostrip \
+    -Dversion=1.1.3 \
+    -Dpackaging=jar \
+    -Dclassifier=sources \
+    -Durl="https://api.bintray.com/maven/vorpal-research/kotlin-maven/kotlin-compiler-nostrip/;publish=1" \
+    -DrepositoryId=bintray-vorpal-research-kotlin-maven
+
+
