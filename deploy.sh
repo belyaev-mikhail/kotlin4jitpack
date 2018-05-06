c01a687c40c6980be52a7901cb6f7c57e3ba663d
Mikhail Belyaev <belyaev@kspt.icc.spbstu.ru>
2018-05-06 19:22:48 +0300

    Up to kotlin 1.2.41

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,30 @@
+: ${JDK:?"Need to set JDK to your current JDK location"}
+
+export JDK_16=$JDK
+export JDK_17=$JDK
+export JDK_18=$JDK
+export JDK_9=$JDK
+
 cd kotlin 
-ant -f update_dependencies.xml 
-ant -f build.xml 
+
+./gradlew :kotlin-compiler:dist
+./gradlew :kotlin-compiler:sourcesJar
+
 mvn deploy:deploy-file \
-    -Dfile=dist/kotlin-compiler-before-shrink.jar \
+    -Dfile=prepare/compiler/build/libs/kotlin-compiler-1.2-SNAPSHOT-before-proguard.jar \
     -DgroupId=org.jetbrains.kotlin \
     -DartifactId=kotlin-compiler-nostrip \
-    -Dversion=1.1.3 \
+    -Dversion=1.2.41 \
     -Dpackaging=jar \
     -Durl="https://api.bintray.com/maven/vorpal-research/kotlin-maven/kotlin-compiler-nostrip/;publish=1" \
     -DrepositoryId=bintray-vorpal-research-kotlin-maven
 
 mvn deploy:deploy-file \
-    -Dfile=dist/kotlin-compiler-sources.jar \
+    -Dfile=prepare/compiler/build/libs/kotlin-compiler-1.2-SNAPSHOT-sources.jar \
     -DgroupId=org.jetbrains.kotlin \
     -DartifactId=kotlin-compiler-nostrip \
-    -Dversion=1.1.3 \
+    -Dversion=1.2.41 \
     -Dpackaging=jar \
     -Dclassifier=sources \
     -Durl="https://api.bintray.com/maven/vorpal-research/kotlin-maven/kotlin-compiler-nostrip/;publish=1" \
     -DrepositoryId=bintray-vorpal-research-kotlin-maven
-
-
